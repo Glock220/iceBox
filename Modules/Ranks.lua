59d6b60971f3316bdac0e830744387c70dc6918e
Glock220 <148919902+Glock220@users.noreply.github.com>
2024-06-01 19:50:23 -0500

    Create Ranks.lua

---
+++ Modules/Ranks.lua
@@ -0,0 +1,14 @@
+return table.freeze({
+	Moderators = table.freeze({
+		687424384,-- Cxrn_45
+		3270554075,-- TheFakeFew
+		1317400706,-- i8mb
+		3607678805,-- equsjd1
+	}),
+	Members = table.freeze({
+		1, -- the blox
+	}),
+	Blacklist = table.freeze({
+		-- Future blacklisted people go here
+	})
+})
